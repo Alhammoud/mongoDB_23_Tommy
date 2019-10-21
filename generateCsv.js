813b94c85cdce37af8ce07d4f0c8364cdbe843e9
alhammoud <>
2019-10-21 14:44:09 +0200

    forEach use to show all records where noxevent

--- generateCsv.js
+++ generateCsv.js
@@ -6,13 +6,28 @@ const Aqmpoint = require("./Aqmpoint");
 mongoose.connect(process.env.MONGOURL, {
     useNewUrlParser: true,
     useCreateIndex: true,
-     useUnifiedTopology: true
+    useUnifiedTopology: true
 });
 
 (async () => {
-    const allPoints = await Aqmpoint.find();
-    console.log(" xxx ", allPoints);
-    
-     } 
+        const allPoints = await Aqmpoint.find();
+        /* 
+        console.log(" xxx ", allPoints); */
+
+        
+        let count = 0;
+        allPoints.forEach(function (element) {
+            if (element.aqm.noxevent > 20) {
+                console.log(element);
+                count++;
+            }
+
+        });
+
+        /*         var query = Aqmpoint.find({ name: /john/i }, null, { skip: 10 });
+         */
+
+        console.log(' all records where noxevent > 20 are : ' + count);
+    }
 
 )()
